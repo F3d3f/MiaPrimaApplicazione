67efb6f929d7bbb1367b8205ecea2fbcbdd03363
Federico <st10567@iismarconipieralisi.it>
2025-02-28 11:26:46 +0100

    aggiunta scroll view e fix errori

--- app/src/main/java/com/example/miaprimaapplicazione/MainActivity.java
+++ app/src/main/java/com/example/miaprimaapplicazione/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         registrazione.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(MainActivity.this, Registrazione.class);
                 startActivity(intent);
             }
         });
